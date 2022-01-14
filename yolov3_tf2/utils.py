5771f89d83232957148eb27eb5855e188f378636
saswat0 <saswatsubhajyotimallick@gmail.com>
2022-01-14 11:35:00 +0530

    added weights folder

--- yolov3_tf2/utils.py
+++ yolov3_tf2/utils.py
@@ -26,6 +26,7 @@ YOLOV3_TINY_LAYER_LIST = [
 
 def load_darknet_weights(model, weights_file, tiny=False):
     wf = open(weights_file, 'rb')
+    major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
 
     if tiny:
         layers = YOLOV3_TINY_LAYER_LIST
